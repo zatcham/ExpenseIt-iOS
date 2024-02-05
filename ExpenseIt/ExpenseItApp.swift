6fa677aa8db72247275fe47089d57573dec5bd98
Prestigeaura <PrestigeAura@gmail.com>
2024-02-05 14:09:35 +0000

    Initial Commit

---
+++ ExpenseIt/ExpenseItApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseItApp.swift
+//  ExpenseIt
+//
+//  Created by George Clarke on 05/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseItApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
